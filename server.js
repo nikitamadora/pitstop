4a188d3a4b75c8ec6bada7f2967d3f9b5144b9bb
nmadora <nikitamadora@gmail.com>
2020-02-28 12:15:12 -0800

    Fix syntax for bodyParser

--- server.js
+++ server.js
@@ -20,19 +20,19 @@ const PORT = process.env.PORT || 4200;
 //       Middleware
 //-----------------------
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true });
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //-----------------------
 //     Views Routes
 //-----------------------
 // TODO Reactivate when we have stubbed out our routes/index.js AND our routes/views.js
-app.use('/', routes.views);
+// app.use('/', routes.views);
 
 //-----------------------
 //      API Routes
 //-----------------------
 // TODO Reactivate when we have our routes/index.js AND routes/api.js
-app.use('/api/v1', routes.api);
+// app.use('/api/v1', routes.api);
 
 //-----------------------
 //      Start Server
