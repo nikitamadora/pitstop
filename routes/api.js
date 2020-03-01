8916a50c360d77bd1df37952c1f2fd745905ae74
nmadora <nikitamadora@gmail.com>
2020-02-29 18:15:45 -0800

    Adds all api routes

--- routes/api.js
+++ routes/api.js
@@ -41,9 +41,34 @@ router.get('/restrooms/:id', (req, res) => {
           res.json(foundRestroom);
      });
 });
-// router.post('/restrooms', ctrl.restrooms.create);
-// router.put('/restrooms/:id', ctrl.restrooms.update);
+
+// New Restroom API Route
+router.post('/restrooms', (req, res) => {
+     db.Restroom.create(req.body, (err, newRestroom) => {
+          if (err) return res.json(err);
+
+          res.json(newRestroom);
+     });
+});
+
+// Update Restroom API Route
+router.put('/restrooms/:id', (req, res) => {
+     db.Restroom.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedRestroom) => {
+          if (err) return res.status(400).json({ status: 400, error:'Something went wrong, please try again.'});
+
+          res.json(updatedRestroom);
+     });
+});
+
 // router.delete('/restrooms/:id', ctrl.restrooms.destroy);
+router.delete('/restrooms/:id', (req, res) => {
+     console.log(`Deleted ${req.params.locationName}`);
+     db.Restroom.findByIdAndDelete(req.params.id, (err, deletedRestroom) => {
+          if (err) return res.status(400).json({ status: 400, error: 'Something went wrong, please try again.' });
+
+          res.json(deletedRestroom);
+     });
+});
 
 // -----------------------
 //      Export Router
