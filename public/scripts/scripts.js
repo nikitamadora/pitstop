40ceacfcd7cbd6556a1922457da49dbee78cfd75
aurelieverrot <aurelie.verrot@gmail.com>
2020-03-02 21:39:57 -0800

    added data from restroom objects into restroom cards

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,7 +1,7 @@
 const API_BASE = '/api/v1';
 const cardContainerEl = document.querySelector('#restroom-cards');
 
-// ----- Script to list restrooms into show.html
+// ----- Script to list the restrooms into show.html
 function getRestrooms() {
   fetch(`${API_BASE}/restrooms`)
   .then(res => res.json())
@@ -25,12 +25,20 @@ function getRestroomTemplate(restroom) {
       </div>
       <div class="card-content">
           <span class="card-title activator grey-text text-darken-4">${restroom.locationName}<i class="material-icons right">more_vert</i></span>
-          <p><a href="#">This is a link</a></p>
+          <p>${restroom.streetAddress}, ${restroom.neighborhood}</p>
       </div>
       <div class="card-reveal">
           <span class="card-title grey-text text-darken-4">${restroom.locationName}<i class="material-icons right">close</i></span>
-          <p>blablabla</p>
+          <p>${restroom.streetAddress}, ${restroom.neighborhood}</p>
+          <p>Directions: ${restroom.directions}</p>
+          <p>Hours of operation: ${restroom.hours}</p>
+          <p>Gender Neutral: ${restroom.genderNeutral}</p>
+          <p>Changing Station: ${restroom.changingStation}</p>
+          <p>Require Purchase: ${restroom.reqPurchase}</p>
+          <p>Sanitary Products Available: ${restroom.sanitaryProducts}</p>
+          <p>Star Rating: ${restroom.starRating}/5</p>
       </div>
     </div>
   `
 };
+
