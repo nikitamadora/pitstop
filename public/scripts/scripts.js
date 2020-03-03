9a62fc53240d00437bd30f77a652a987e1895ce4
aurelieverrot <aurelie.verrot@gmail.com>
2020-03-02 17:01:38 -0800

    started basic functions to list restrooms into show.html, got error on DOM manip

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,1 +1,34 @@
-console.log('Sanity check on Show.html')
+const API_BASE = '/api/v1';
+const cardContainerEl = document.querySelector('col s9');
+
+// ----- Script to list restrooms into show.html
+function getRestrooms(e) {
+  event.preventDefault();
+  fetch(`${API_BASE}/restrooms`)
+  .then (res => res.json())
+  .then (json => render(json))
+};
+getRestrooms();
+
+function render(restroom) {
+  
+  cardContainerEl.insertAdjacentHTML('beforeend', getRestroomTemplate(restroom))
+};
+
+function getRestroomTemplate(restroom) {
+  return `
+      <div class="card">
+      <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="https://picsum.photos/300/400">
+      </div>
+      <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+          <p><a href="#">This is a link</a></p>
+      </div>
+      <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+          <p>${restroom.location}</p>
+      </div>
+    </div>
+  `
+};
