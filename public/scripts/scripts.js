326a6bb88f75dbd57fbf7d2f604e14174fab792e
nmadora <nikitamadora@gmail.com>
2020-03-02 20:18:26 -0800

    Adds form markup to new.html

--- public/scripts/scripts.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-     const elems = document.querySelectorAll('.collapsible');
-     const instances = M.Collapsible.init(elems, options);
-   });
